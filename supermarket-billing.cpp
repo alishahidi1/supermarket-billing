3733e8672d1789786d7e7aecf0b0d3cb5ca9fcd8
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-02-14 14:53:21 -0500

    remove function is defined

--- supermarket-billing.cpp
+++ supermarket-billing.cpp
@@ -238,3 +238,40 @@ void shopping::edit(){
     }
 
 }
+
+void shopping::rem(){
+    fstream data, data1;
+    int pkey;
+    int token = 0;
+    cout<<"\n\n\t Delete product";
+    cout<<"\n\n\t Enter the product code: ";
+    cin>>pkey;
+    data.open("database.txt", ios::in);
+
+    if(!data){
+        cout<<"File doesn't exist!";
+    }
+    else{
+        data1.open("database1.txt",ios::app | ios::out);
+        data>>pcode>>pname>>price>>dis;
+
+        while(!data.eof()){
+            if(pcode == pkey){
+                cout<<"\n\n\t Product deleted successfully";
+                token++;
+            }
+            else{
+                data1<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<"\n";
+            }
+            data>>pcode>>pname>>price>>dis;
+        }
+        data.close();
+        data1.close();
+        remove("database.txt");
+        rename("database1.txt", "database.txt");
+
+        if(token == 0){
+            cout<<"\n\n Record not found!";
+        }
+    }
+}
