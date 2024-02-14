989e88bd11a2b662bea56f4fc87941089b9bc97e
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-02-14 14:29:36 -0500

    edit function is defined

--- supermarket-billing.cpp
+++ supermarket-billing.cpp
@@ -179,5 +179,62 @@ void shopping::add(){
         data.close();
     }
     }
-    cout<<"\n\n\t\t Record inserted! ";
+    cout<<"\n\n\t\t Record inserted!";
+}
+
+void shopping::edit(){
+    
+    fstream data, data1;
+    int pkey;
+    int token = 0;
+    int c;
+    float p;
+    float d;
+    string n;
+
+    cout<<"\n\t\t\t Modify the record";
+    cout<<"\n\t\t\t Product mode:";
+    cin>>pkey;
+
+    data.open("database.txt", ios::in);
+    if(!data){
+        cout<<"\n\n File doesn't exist!";
+    }
+    else{
+        
+        data1.open("database1.txt", ios::app | ios::out);
+        
+        data>>pcode>>pname>>price>>dis;
+        while(!data.eof()){
+            if(pkey == pcode){
+                cout<<"\n\t\t Product new code: ";
+                cin>>c;
+                cout<<"\n\t\t Name of the product: ";
+                cin>>n;
+                cout<<"\n\t\t Price: ";
+                cin>>p;
+                cout<<"\n\t\t Discount: ";
+                cin>>d;
+
+                data1<<" "<<c<<" "<<n<<" "<<p<<" "<<d<<"\n";
+                
+                cout<<"\n\n\t\t Record edited";
+                token++;
+            }
+            else{
+                data1<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<"\n";
+            }
+            data>>pcode>>pname>>price>>dis;
+        }
+        data.close();
+        data1.close();
+
+        remove("database.txt");
+        rename("database1.txt", "database.txt");
+
+        if(token == 0){
+            cout<<"\n\n Record not found!";
+        }
+    }
+
 }
